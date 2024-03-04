26ad1b8300118541d84da4ac5404ecadfcdf8cbb
Elias Gill <eliasgill42@gmail.com>
2024-03-04 19:20:30 -0300

    mas notas y proyecto backend

--- Backend/proyecto/src/main/java/py/com/progweb/prueba/rest/ApplicationConfig.java
+++ Backend/proyecto/src/main/java/py/com/progweb/prueba/rest/ApplicationConfig.java
@@ -1,8 +1,16 @@
 package py.com.progweb.prueba.rest;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
 import javax.ws.rs.core.Application;
+
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
 
 @ApplicationPath("/")
 public class ApplicationConfig extends Application {
+    @GET
+    public void reqUser(HttpRequest req, HttpResponse resp) {
+        resp.getHeaderNames
+    }
 }
